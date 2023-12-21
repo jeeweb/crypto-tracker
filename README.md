c592b85046dd878803cbfe98dc01c534b542780d
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-12-22 00:39:49 +0900

    Setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Crypto Tracker
+
+Coin Tracking Application
+React, Typescript, Styled-component 연습하기
+
+## 실행
+
+```
+npm start
+```
---
+++ src/theme.ts
@@ -0,0 +1,6 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+};
