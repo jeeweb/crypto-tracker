720a3d00b8c43c72d9fa2cdc277488a5659749ad
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-12-22 04:44:12 +0900

    feat: coin list 화면 작업
    add loading spinner
    fetch data (axios)
    style

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,4 +1,150 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const LoadingSpinner = styled.span`
+  display: block;
+  position: relative;
+  width: 48px;
+  height: 48px;
+  margin: auto;
+  border-radius: 50%;
+  animation: rotate 1s linear infinite;
+  &:before {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    inset: 0px;
+    border-radius: 50%;
+    border: 5px solid ${(props) => props.theme.textColor};
+    animation: prixClipFix 2s linear infinite;
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes prixClipFix {
+    0% {
+      clip-path: polygon(50% 50%, 0 0, 0 0, 0 0, 0 0, 0 0);
+    }
+    25% {
+      clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 0, 100% 0, 100% 0);
+    }
+    50% {
+      clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 100% 100%, 100% 100%);
+    }
+    75% {
+      clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%);
+    }
+    100% {
+      clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 0);
+    }
+  }
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  margin-bottom: 10px;
+  color: ${(props) => props.theme.textColor};
+  background: rgba(255, 255, 255, 0.04);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.02);
+  transition: 0.18s;
+  a {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    padding: 20px;
+    font-size: 20px;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    background: rgba(255, 255, 255, 0.05);
+    border: 1px solid rgba(33, 146, 255, 0.8);
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Img = styled.img`
+  width: 36px;
+  height: 25p36x;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 function Coins() {
-  return <h1>Coins</h1>;
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  const getCoins = async () => {
+    const response = await axios("https://api.coinpaprika.com/v1/coins");
+    setCoins(response.data.slice(0, 100));
+    setLoading(false);
+  };
+  useEffect(() => {
+    getCoins();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>Coin</Title>
+      </Header>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                <span>{coin.name}</span>
+              </Link>
+            </Coin>
+          ))}
+          <Coin></Coin>
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
 export default Coins;
